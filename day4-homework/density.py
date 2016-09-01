31a36f9d98cad6dd92aa6e25bc3374af986f7a22
Daria Lukasz <daria.lukasz93@gmail.com>
2016-09-01 18:28:30 -0400

    How to make density plot

---
+++ day4-homework/density.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+
+from sklearn import datasets
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+
+from scipy.stats import gaussian_kde
+
+
+data = pd.read_csv(sys.argv[1], sep="\t")
+df_raw = data["FPKM"] + 1
+#df_raw2 = df_raw["FPKM"]
+df_log = np.log10(df_raw)
+
+
+
+density = gaussian_kde(df_log)
+
+# xs = np.linspace(0,8,200)
+# density.covariance_factor = lambda : .25
+# density._compute_covariance()
+# plt.plot(xs,density(xs))
+xs = np.linspace(-5, 5, 200)
+
+plt.figure()
+plt.plot(xs, density(xs))
+plt.title("Density Plot")
+plt.xlabel("FPKM values")
+plt.ylabel("Density")
+plt.savefig("density.png")
+plt.close()
