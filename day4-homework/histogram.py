4a3059a76e731500c600dfe71e8ed4fe842ef4bc
Daria Lukasz <daria.lukasz93@gmail.com>
2016-09-01 17:26:26 -0400

    How to make histogram

---
+++ day4-homework/histogram.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+from sklearn import datasets
+import numpy as np
+
+df = pd.read_csv(sys.argv[1], sep="\t")
+
+
+df_roi = df["FPKM"] > 0
+df_FPKM = df[df_roi]
+fpkm_values = np.log10(df_FPKM["FPKM"])
+print fpkm_values
+#mean = fpkm_values.mean()
+#print mean
+
+plt.figure()
+plt.hist(fpkm_values, bins = 100)
+plt.title("FPKM values for SRR072893")
+plt.xlabel("FPKM value")
+plt.ylabel("Number of occurrences")
+plt.savefig("histogram.png")
+
+plt.close()
