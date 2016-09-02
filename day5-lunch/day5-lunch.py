bad8449c750494012d359ef6a95a70672d68a237
Daria Lukasz <daria.lukasz93@gmail.com>
2016-09-02 14:45:03 -0400

    Updated python lunch script

--- day5-lunch/day5-lunch.py
+++ day5-lunch/day5-lunch.py
@@ -15,6 +15,7 @@ for item in df.itertuples():
     chrom = item[2]
     chrom_list = ["2L", "2R", "3L", "3R", "4", "X"]
     t_name = item[6]
+    fpkm = item[12]
     if item[3] == "+":
         p_st = item[4] - 500
         p_end = item[4] + 500
@@ -24,7 +25,7 @@ for item in df.itertuples():
     if chrom not in chrom_list:
         continue
     else:
-        print "%s\t%d\t%d\t%s" % (chrom, p_st, p_end, t_name)
+        print "%s\t%d\t%d\t%s\t%d" % (chrom, p_st, p_end, t_name, fpkm)
  
     
 
